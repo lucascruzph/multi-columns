786ed439713930aade46ae45e490e0753cac397b
Lucas Cruz <lucas_empresa@yahoo.com.br>
2024-01-09 02:44:44 -0300

    M4A2 - Refining the project

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6c1e26b0b376e376a91e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3451eb84a2be20f80964');
